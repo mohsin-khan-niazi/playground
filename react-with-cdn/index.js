0bcd38271b17d44f5075bc44c2e1d281e3ca17ba
Mohsin Niazi <mohsinkniazi22@gmail.com>
2023-12-23 01:41:21 +0500

    basic react app created
    unstyled navbar created with react and packages imported through CDN

---
+++ react-with-cdn/index.js
@@ -0,0 +1,17 @@
+const navbar = (
+  <nav>
+    <h1>
+      Lollapalloza.com
+    </h1>
+    <ul>
+      <li>About</li>
+      <li>Contact</li>
+      <li>Pricing</li>
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(
+  navbar,
+  document.getElementById('root')
+)  
